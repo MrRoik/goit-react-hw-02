96cb247b2c7ecf1232dbb3e8e5597d0cb7163381
MrRoik <138239220+MrRoik@users.noreply.github.com>
2024-01-20 11:00:00 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,69 +6,35 @@ import { Notification } from './Notification/Notification';
 
 
 export const App = () => {
-  const [values, setValues] = useState({
+  const [values, setValues] = useState(() => JSON.parse(localStorage.getItem('saved-values')) || {
     good: 0,
     neutral: 0,
     bad: 0
-  });
-
-  //const [clicks, setClicks] = useState(0);
-  const [clicks, setClicks] = useState(() => {
-    const savedClicks = window.localStorage.getItem("saved-clicks");
-    if (savedClicks !== null) {
-      return JSON.parse(savedClicks);
-    }
-    return 0;
-  });
-
-  useEffect(() => {
-      window.localStorage.setItem("saved-clicks", JSON.stringify({ values }));
-  }, [values]);
+  })
 
   const onUpdateFeedback = option => {
     setValues({
       ...values,
       [option]: values[option] + 1
     });
-    
-    setClicks(clicks + 1);
   };
   
-    const onReset = () => {
+  const onReset = () => {
     setValues({
-      ...values,
       good: 0,
       neutral: 0,
       bad: 0
     });
-    
-    setClicks(0);
   };
+
+   useEffect(() => {
+    window.localStorage.setItem('saved-values', JSON.stringify(values));
+  }, [values]);
   
-  /*const updateGood = () => {
-			setValues({
-			...values,
-			good: values.good + 1
-		});
-  };
-
-  const updateNeutral = () => {
-			setValues({
-			...values,
-			neutral: values.neutral + 1
-		});
-  };
-
-  const updateBad = () => {
-      setValues({
-      ...values,
-      bad: values.bad + 1
-    });
-  };*/
-
-  const isHidden = clicks === 0;
 
   const totalFeedback = values.good + values.neutral + values.bad;
+
+  const isHidden = totalFeedback === 0
 
   const positiveFeedback = Math.round(((values.good + values.neutral) / totalFeedback) * 100);
 
